02451d7c57ae640099db1c456fbabb5bfd06a1be
Michel Oliveira <micheltdoliveira@gmail.com>
2022-04-03 13:43:07 -0300

    removing email confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -18,16 +18,10 @@ ActiveRecord::Schema.define(version: 2022_04_01_212618) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index [nil], name: "index_users_on_unlock_token", unique: true
   end
 
 end
